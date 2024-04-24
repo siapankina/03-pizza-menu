cad830a9860ae99cebc502a57b1f4a63b15f490f
Anastasia  Pankina <anastasiapankina@Anastasias-MacBook-Pro.local>
2024-04-24 10:24:50 +0800

    adding react fragment

--- src/index.js
+++ src/index.js
@@ -73,11 +73,17 @@ function Menu() {
     <main className="menu">
       <h2>Our menu</h2>
       {weHavePizzas ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObject={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <React.Fragment>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObject={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </React.Fragment>
       ) : (
         <p>We are still working on our menu. Please come back later</p>
       )}
