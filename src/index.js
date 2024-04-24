38075c5ef717cb1d47e4d56e63dfb3f760777857
Anastasia  Pankina <anastasiapankina@Anastasias-MacBook-Pro.local>
2024-04-24 10:37:31 +0800

    final touch on css

--- src/index.js
+++ src/index.js
@@ -93,15 +93,12 @@ function Menu() {
 
 function Pizza({ pizzaObject }) {
   return (
-    <li
-      className="pizza"
-      //   className={{props.pizzaObject.soldOut ? "pizza.sold-out" : ""}}
-    >
+    <li className={`pizza ${pizzaObject.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObject.photoName} alt={pizzaObject.photoName} />
       <div>
         <h3>{pizzaObject.name}</h3>
         <p>{pizzaObject.ingredients}</p>
-        <span>{pizzaObject.price}</span>
+        <span>{pizzaObject.soldOut ? "SOLD OUT " : pizzaObject.price}</span>
       </div>
     </li>
   );
